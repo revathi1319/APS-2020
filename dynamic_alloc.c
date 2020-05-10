20295b57bdd5ffcef45cbffa34fb0e6c0836a1d9
revathi1319 <60294518+revathi1319@users.noreply.github.com>
2020-05-10 22:55:56 +0530

    Create dynamic_alloc.c

---
+++ dynamic_alloc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+		int *iVar;
+		
+		iVar=(int*)malloc(sizeof(int));
+		
+		printf("Now, input an integer value: ");
+		scanf("%d",iVar);
+		
+		printf("Great!!! you entered: %d.\n",*iVar);
+		
+		free(iVar);
+		return 0;
+}
