a5ecc17fed74aec5bc7485f06f0f36d285a06713
revathi1319 <60294518+revathi1319@users.noreply.github.com>
2020-05-11 16:13:46 +0530

    Create adashop2.py

---
+++ adashop2.py
@@ -0,0 +1,29 @@
+def printAll():
+    print(1,1,sep=" ")
+    print(8,8,sep=" ")
+    print(7,7,sep=" ")
+    print(8,6,sep=" ")
+    print(3,1,sep=" ")
+    print(1,3,sep=" ")
+    print(6,8,sep=" ")
+    print(5,7,sep=" ")
+    print(8,4,sep=" ")
+    print(5,1,sep=" ")
+    print(1,5,sep=" ")
+    print(4,8,sep=" ")
+    print(3,7,sep=" ")
+    print(8,2,sep=" ")
+    print(7,1,sep=" ")
+    print(1,7,sep=" ")
+    print(2,8,sep=" ")
+
+for _ in range(int(input())):
+    r,c=map(int,input().split())
+
+    print(19)
+
+    print(c,r,sep=" ")
+    tmp1=(r+c)//2
+    print(tmp1,tmp1,sep=" ")
+
+    printAll()
